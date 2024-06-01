b6ff5e141b1a082d57e9582562bf83950a789667
Jack Mangione <jmango201@gmail.com>
2024-06-01 12:39:43 -0400

    converted data matrix to struct

--- mnist.c
+++ mnist.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "mnist.h"
-#define MAX_LINE_LENGTH 100000
 
-float ** read_csv(const char * filename, int *num_rows, int *num_cols) {
+Matrix read_csv(const char *filename) {
+  Matrix data;
+
   FILE *fp_orig = fopen(filename, "r");
   if (fp_orig == NULL) {
     printf("Error Opening File %s\n", filename);
-    return NULL;
+    return data;
   }
 
   char line[MAX_LINE_LENGTH];
@@ -16,7 +14,7 @@ float ** read_csv(const char * filename, int *num_rows, int *num_cols) {
   int max_tokens = 0;
   FILE *fp = fp_orig;
 
-  // each line in the MNIST csv file is a row
+  // each line in the MNIST csv file is a row containing all info for one pattern 
   while (fgets(line, MAX_LINE_LENGTH, fp)) {
       num_lines ++;
       int num_tokens = 1;
@@ -29,9 +27,9 @@ float ** read_csv(const char * filename, int *num_rows, int *num_cols) {
   }
 
   // memory for 2d array
-  float **data = (float**)calloc(num_lines, sizeof(float*));
+  float **arr = (float**)calloc(num_lines, sizeof(float*));
   for (int i = 0; i < num_lines; i++) {
-    data[i] = (float*)calloc(max_tokens, sizeof(float));
+    arr[i] = (float*)calloc(max_tokens, sizeof(float));
   }
 
   fp = fp_orig;
@@ -40,38 +38,37 @@ float ** read_csv(const char * filename, int *num_rows, int *num_cols) {
     char *token = strtok(line, ",");
     int col = 0;
     while (token != NULL) { // iterates through every entry (col) of the selected row 
-      data[row][col] = atof(token);
+      arr[row][col] = atof(token);
       token = strtok(NULL, ",");
       col++;
     }
     row++;
   }
 
-  fclose(fp);
-  *num_rows = num_lines; 
-  *num_cols = max_tokens;
+  fclose(fp_orig);
+  data.mat = arr;
+  data.nrows = num_lines; 
+  data.ncols = max_tokens;
   return data;
 }
 
-void remove_first_row(float*** arr, int *nrows, int ncols) {
-  float **orig_arr = *arr; 
-  int new_rows = *nrows - 1;
+void remove_first_row(Matrix *data) {
+  int new_rows = data->nrows - 1;
   float **new_arr = (float **)calloc(new_rows, sizeof(float *));
   for (int i = 0; i < new_rows; i++) {
-      new_arr[i] = (float *)calloc(ncols, sizeof(float)); 
-      memcpy(new_arr[i], orig_arr[i + 1], ncols * sizeof(float));
+      new_arr[i] = (float *)calloc(data->ncols, sizeof(float)); 
+      memcpy(new_arr[i], data->mat[i + 1], data->ncols * sizeof(float));
   }
-  free_arr(orig_arr, *nrows);
-  *arr = new_arr;
-  *nrows = new_rows;
-
+  free_arr(*data);
+  data->mat = new_arr;
+  data->nrows = new_rows;
 }
 
-void free_arr(float **arr, int nrows) {
-    for (int i = 0; i < nrows; i++) {
-        free(arr[i]);
+void free_arr(Matrix arr) {
+    for (int i = 0; i < arr.nrows; i++) {
+        free(arr.mat[i]);
     }
-    free(arr);
+    free(arr.mat);
 }
 
 /*
@@ -88,22 +85,24 @@ void init_weights(Matrix *W) {
 
 
 int main(int argc, char *argv[]) {
-  int n_rows, n_cols;
-  float **data = read_csv("MNIST_train.csv", &n_rows, &n_cols);
-  printf("rows: %d, cols: %d\n", n_rows, n_cols);
-  remove_first_row(&data, &n_rows, n_cols);
-  printf("rows: %d, cols: %d\n", n_rows, n_cols);
+
+  Matrix data = read_csv("MNIST_train.csv");
+  printf("rows: %d, cols: %d\n", data.nrows, data.ncols);
+  for (int i = 0; i < 100; i++) {
+      remove_first_row(&data);
+      printf("rows: %d, cols: %d\n", data.nrows, data.ncols);
+  }
 
   
-  /*
-  Matrix W1 = { .num_rows = n_rows, .num_cols = nodes_L2 } 
+  //Matrix W1 = { .num_rows = n_rows, .num_cols = nodes_L2 } 
 
+  /*
   init_weights(&W1);
   init_weights(&W2);
   init_weights(&W3);
   */
 
-  free_arr(data, n_rows);
+  free_arr(data);
   return 0;
 }
 
--- mnist.h
+++ mnist.h
@@ -1,15 +1,18 @@
-
-void remove_first_row(float ***arr, int *n_rows, int n_cols);
-float ** read_csv(const char *filename, int *num_rows, int *num_cols);
-int main(int argc, char *argv[]);
-void free_arr(float **arr, int rows);
-void init_weights(float ***W1, float ***W2, float ***W3);
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE_LENGTH 100000
 
 typedef struct {
     int nrows; 
     int ncols; 
-    float **arr;
-} matrix;
+    float **mat;
+} Matrix;
+
+Matrix read_csv(const char *filename);
+void free_arr(Matrix arr);
+void remove_first_row(Matrix *data);
+
+
 
 
