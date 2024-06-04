09ecb875d3b61193f93677e0438f776436cb634b
Jack Mangione <jmango201@gmail.com>
2024-06-04 14:35:46 -0400

    created separate file for handling input and reading csv. Created Makefile

---
+++ Makefile
@@ -0,0 +1,25 @@
+CC= gcc
+CFLAGS= -Wall -g -lm -std=gnu99
+LD= gcc
+LDFLAGS = -L. -lm
+TARGETS = mnist clean_obj
+
+all: $(TARGETS)
+
+
+mnist.o: mnist.c mnist.h
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+input.o: input.c mnist.h
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+mnist: mnist.o input.o
+	$(LD) $(LDFLAGS) -o $@ $^
+
+
+clean:
+	@rm -f *.o mnist
+
+clean_obj:
+	@rm -f *.o
+
--- mnist.c
+++ mnist.c
@@ -1,72 +1,5 @@
 #include "mnist.h"
 
-// Function to read a CSV file into a 2D array of floats
-Matrix read_csv(const char* filename) {
-    Matrix data_matrix;
-    data_matrix.nrows = 41999;
-    data_matrix.ncols = 785;
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        fprintf(stderr, "Could not open file %s for reading\n", filename);
-        return data_matrix;
-    }
-
-    // Allocate memory for the 2D array
-    float **data = (float **)malloc(data_matrix.nrows* sizeof(float *));
-    if (!data) {
-        fprintf(stderr, "Memory allocation failed\n");
-        fclose(file);
-        return data_matrix;
-    }
-
-    for (int i = 0; i < data_matrix.nrows; i++) {
-        data[i] = (float *)malloc(data_matrix.ncols * sizeof(float));
-        if (!data[i]) {
-            fprintf(stderr, "Memory allocation failed\n");
-            // Free previously allocated memory before returning
-            for (int j = 0; j < i; j++) {
-                free(data[j]);
-            }
-            free(data);
-            fclose(file);
-            return data_matrix;
-        }
-    }
-
-    char line[16000]; // Large enough buffer to hold one line of the CSV file
-
-    for (int i = 1; i < data_matrix.nrows + 1; i++) {
-        if (!fgets(line, sizeof(line), file)) {
-            fprintf(stderr, "Error reading line %d\n", i);
-            // Free allocated memory before returning
-            for (int j = 0; j <= i; j++) {
-                free(data[j]);
-            }
-            free(data);
-            fclose(file);
-            return data_matrix;
-        }
-        char *token = strtok(line, ",");
-        for (int j = 0; j < data_matrix.ncols; j++) {
-            if (token) {
-                data[i-1][j] = strtof(token, NULL);
-                token = strtok(NULL, ",");
-            } else {
-                fprintf(stderr, "Error parsing line %d\n", i);
-                // Free allocated memory before returning
-                for (int k = 0; k <= i; k++) {
-                    free(data[k]);
-                }
-                free(data);
-                fclose(file);
-                return data_matrix;
-            }
-        }
-    }
-    data_matrix.mat = data;
-    fclose(file);
-    return data_matrix;
-}
 
 Matrix transpose_matrix(Matrix *arr) {
     float **new_matrix = calloc(arr->ncols, sizeof(float*));
@@ -351,6 +284,7 @@ int main(int argc, char *argv[]) {
     // forward pass, prints average for all training examples
     // it will become necessary to write a function that returns a matrix containing x number of training examples
     Layers layers = forward_pass(&X_test, &W1, &W2, &W3);
+    // layers = forward_pass(&X_test, &W1, &W2, &W3);
 
 
     // cleanup
