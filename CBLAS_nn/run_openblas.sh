42d8cb3e2e4b8664d72b2d93aeea43e410f222ee
Jack Mangione <jmango201@gmail.com>
2024-11-19 19:34:54 -0500

    added shell scripts for running executables

---
+++ CBLAS_nn/run_openblas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export OPENBLAS_NUM_THREADS=8
+
+if [ ! -e "mnist_nn" ]; then
+    echo "executable not found. making... "
+    make clean
+    make
+fi
+
+if [ "$?" -ne "0" ]; then
+    echo ""
+    echo "Compilation of Openblas C implementation failed"
+    exit 1
+fi
+
+LD_PRELOAD=~/local/openblas/lib/libopenblas.so ./mnist_nn
